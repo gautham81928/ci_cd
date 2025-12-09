542f0ec8623646624726b113bb80d90c2f5a03d9
Gautham Viswanathan <156958827+gautham81928@users.noreply.github.com>
2025-12-09 17:36:19 +0000

    Added more operations

--- src/app.py
+++ src/app.py
@@ -1,2 +1,31 @@
+import math
+
 def add (a,b):
     return a + b
+    
+def subtract (a,b):
+    return a - b
+
+def multiply (a,b):
+    return a * b
+
+def divide (a,b):
+    return a / b
+
+def square (a):
+    return a * a
+
+def log (a):
+    return math.log(a)
+
+def sin (a):
+    return math.sin(a)
+
+def cos (a):
+    return math.cos(a)
+
+def square_root (a):
+    return math.sqrt(a)
+
+def percentage (a,b):
+    return ((divide(a,b)) * 100)
