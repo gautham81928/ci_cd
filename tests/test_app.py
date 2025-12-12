c4d52d5ecfca487e43a048906a7efe8609753b4a
Gautham Viswanathan <156958827+gautham81928@users.noreply.github.com>
2025-12-12 03:28:51 +0000

    Edited app.py and test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,13 +5,13 @@ root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 from app import add
-from app import subtract
-from app import multiply
-from app import divide
+from app import subtraction
+from app import multiplication
+from app import division
 from app import square
 from app import log
-from app import sin
-from app import cos 
+from app import sine
+from app import cosine
 from app import square_root
 from app import percentage
 
@@ -22,22 +22,22 @@ def test_add2():
     assert add(5, 6) != 10
 
 def test_subtract():
-    assert subtract(6, 5) == 1
+    assert subtraction(6, 5) == 1
 
 def test_subtract2():
-    assert subtract(10, 4) != 5
+    assert subtraction(10, 4) != 5
 
 def test_multiply():
-    assert multiply(5, 6) == 30
+    assert multiplication(5, 6) == 30
 
 def test_multiply2():
-    assert multiply(5, 6) != 25
+    assert multiplication(5, 6) != 25
 
 def test_divide():
-    assert divide(6, 3) == 2
+    assert division(6, 3) == 2
 
 def test_divide2():
-    assert divide(4, 2) != 7
+    assert division(4, 2) != 7
 
 def test_square():
     assert square(2) == 4
@@ -52,16 +52,16 @@ def test_log2():
     assert log(1) != 1
 
 def test_sin():
-    assert sin(90) == 1
+    assert sine(90) == 1
 
 def test_sin2():
-    assert sin(90) != 2
+    assert sine(90) != 2
 
 def test_cos():
-    assert abs(cos(90)) < 1e-9
+    assert abs(cosine(90)) < 1e-9
 
 def test_cos2():
-    assert cos(90) != 1
+    assert cosine(90) != 1
 
 def test_square_root():
     assert square_root(4) == 2
